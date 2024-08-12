39f8db14cc1e4ddf3dc273b7fe337717357532bc
lis :v <fraimanfederica@gmail.com>
2024-08-12 17:08:32 -0300

    Add files via upload
    todos los trabas que me comi, y cuando el precio yo les discuti, siempre me cagaban a trompadas porque yo me hacia el rata y la plata no les di

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Técnica 2</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=DM+Serif+Display&family=IM+Fell+English:ital@1&display=swap"
+        rel="stylesheet">
+</head>
+<body>
+    <p>"Técnica 2, más que una institución"</p>
+
+    <p1> Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam ut magnam quibusdam ipsum, quia distinctio, autem iure aspernatur, inventore illum sint. Tenetur, libero numquam! Commodi quaerat doloribus harum voluptatibus aperiam!</p1>
+    
+</div>
+</body>
+</html>
